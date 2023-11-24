de8e781511ebcaf7797b25ac818691800d851e2e
Mikael Hirn <mikael.hirn@spottio.com>
2023-11-24 12:19:12 +0200

    Moved static markers to parent. Created marker emitting from child to parent.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <navBar />
-    <addMarker :markers = "markers"/>
+    <addMarker :markers = "markers" @marker-add="receiceNewMarker"/>
     <!-- <addMarker /> -->
     <navBarBottom />
 
@@ -11,13 +11,27 @@ import navBar from './components/NavBar.vue'
 import navBarBottom from './components/NavBar_bottom.vue'
 import addMarker from './views/AddMarkerToMap.vue'
 
-const markers = localStorage.getItem('markers');
+// const markers = localStorage.getItem('markers');
 
-localStorage.setItem('markers', JSON.stringify(['60.3112373549543, 25.38869619369507','60.24289554387395, 24.978044629096985']));
+// localStorage.setItem('markers', JSON.stringify(['60.3112373549543, 25.38869619369507','60.24289554387395, 24.978044629096985']));
 
-// const markers = [
-//     '60.3112373549543, 25.38869619369507',
-//     '60.24289554387395, 24.978044629096985',
-// ];
+const markers = [
+    {
+        id: 1,
+        loc: [60.3112373549543, 25.38869619369507],
+        name: 'Box Cafe & Grill',
+        type: ['Burger']
+    },
+    {
+        id: 2,
+        loc: [60.24289554387395, 24.978044629096985],
+        name: 'Mc Donalds - Tikkurila',
+        type: ['Burger']
+    },
+];
+
+function receiceNewMarker(resdata) {
+    console.log(resdata);
+}
 
 </script>
--- src/views/AddMarkerToMap.vue
+++ src/views/AddMarkerToMap.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container-fluid mx-auto text-center overflow-scroll" style="padding-top: 80px; padding-bottom: 100px;">
         <div class="row">
-            <h3 class="pruple-text-emphasis">Lis&auml;&auml; ravintola kartalle</h3>
+            <h3 class="pruple-text-emphasis">Lis&auml;&auml; ravintola</h3>
             
             <div style="height:300px; width:100%">
                 <l-map ref="map" v-model:zoom="zoom" :use-global-leaflet="false" :center="[60.3112373549543, 25.38869619369507]" @click="addMarker" :options="{zoomControl:false}" >
@@ -11,19 +11,19 @@
                         name="OpenStreetMap">
                     </l-tile-layer>
                     <l-marker 
-                        v-for="marker, index in markers"
-                        :lat-lng="marker.marker" :key="index">
+                        v-for="marker, index in props.markers"
+                        :lat-lng="marker.loc" :key="index">
                         <l-popup>
-                            {{ marker.id }}
+                            {{ marker.name }}
                         </l-popup>
                     </l-marker>
                 </l-map>
             </div>
             
-            <form @submit.prevent>
+            <form @submit.prevent="addNewRestaurant()">
 
                 <label class="px-2 py-2" for="coord">Koordinaatit </label><br />
-                <input type="text" v-model="coord" id="coord"/><br />
+                <input type="text" v-model="coord" id="coord" placeholder="Ei asetettu"/><br />
 
                 <label class="px-2 py-2" for="name">Nimi </label><br />
                 <input type="text" v-model="name" placeholder="Ravintolan nimi" /><br />
@@ -36,7 +36,7 @@
                 <label class="px-4 py-4" for="burger">Burger</label>
                 <input type="checkbox" value="burger" v-model="checkedTypes" /><br />
 
-                <button class="btn btn-secondary px-5" name="submit" @click="addmarker">Lis&auml;&auml; ravintola</button>
+                <button class="btn btn-secondary px-5" name="submit">Lis&auml;&auml; ravintola</button>
             </form>
         </div>
     </div>
@@ -47,46 +47,51 @@ import { onMounted, ref } from 'vue';
 import "leaflet/dist/leaflet.css";
 import {LMap, LTileLayer, LMarker, LPopup} from "@vue-leaflet/vue-leaflet"
 
-// const props = defineProps({
-//     markers: String
-// })
+const props = defineProps({
+    markers: Object
+});
 
-// console.log(this.map.getBounds())
-
-const markers = [
-    {
-        id: 1,
-        marker: [60.3112373549543, 25.38869619369507]
-    },
-    {
-        id: 2,
-        marker: [60.24289554387395, 24.978044629096985]
-    },
-];
-
-const name = ref('');
-const coord = ref();
+let name = ref('');
+let coord = ref('');
 const checkedTypes = ref([]);
 const zoom = 15;
 
-function addMarker() {
+const emit = defineEmits('marker-add');
 
+onMounted( () => {
+    //Set zoom to view all markers
+    // getExistingMarkers()
+});
+
+function addNewRestaurant() {
+    const newMarker =
+        {
+            id: 3,
+            loc: coord.value,
+            name: name.value,
+            type: checkedTypes.value
+        };
+
+    //TODO: Emit new marker to parent
+    emit('marker-add', newMarker);
     //Add marker to local storage
     // const newValues = localStorage.getItem('markers') + coord;
     // localStorage.setItem('markers_new', JSON.stringify(newValues));
     
 }
 
-onMounted( () => {
-    console.log('mounted');
-    //Set zoom to view all markers
+function addMarker(event){
+    console.log(event.latlng.lat + ' ' + event.latlng.lng);
+    coord = event.latlng.lat + ' ' + event.latlng.lng;
+    name = 'Temp';
     
-    // updateExistingMarkers()
-});
+
+}
+
 
 //TODO: Show existing markers on map when loaded for the fist time
 
-// function updateExistingMarkers(){
+// function getExistingMarkers(){
 //     const temp = props.markers;
 //     return temp;
 // }
